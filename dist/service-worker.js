6205674bc49b1626a3cac40e3049fcb06883702a
dkramskoj <kramskoj.d@brights.cloud>
2020-05-20 18:49:52 +0300

    test commit

---
+++ dist/service-worker.js
@@ -0,0 +1,25 @@
+importScripts("/precache-manifest.81c809ab0b2572a634b4c770e0549829.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front"
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [new RegExp("/graphql"), new RegExp("/dashboard")]
+  }
+);
+
