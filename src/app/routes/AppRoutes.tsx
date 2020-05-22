d7fc7374b80ee12780c752dd6b93952a4e4bf966
dkramskoj <kramskoj.d@brights.cloud>
2020-05-22 19:00:28 +0300

    public path added to routes

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -15,29 +15,33 @@ import { SearchPage } from "../../views/Search";
 import { CartPage, CheckoutPage, PasswordReset, ThankYouPage } from "@pages";
 
 import * as paths from "./paths";
+const publicURI = process.env.NODE_ENV === "development" ? '' : 'saleor-front';
 
+console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log(process.env.NODE_ENV === "development")
+console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 export const Routes: React.FC = () => (
-  <Switch>
-    <Route exact path={paths.baseUrl} component={HomePage} />
-    <Route path={paths.searchUrl} component={SearchPage} />
-    <Route path={paths.categoryUrl} component={CategoryPage} />
-    <Route path={paths.collectionUrl} component={CollectionPage} />
-    <Route path={paths.productUrl} component={ProductPage} />
-    <Route path={paths.cartUrl} component={CartPage} />
-    <Route path={paths.checkoutLoginUrl} component={CheckoutLogin} />
-    <Route path={paths.pageUrl} component={ArticlePage} />
-    <Route path={accountPaths.baseUrl} component={UserAccount} />
-    <Route path={accountPaths.userOrderDetailsUrl} component={OrderDetails} />
-    <Route path={paths.guestOrderDetailsUrl} component={OrderDetails} />
-    <Route path={paths.accountUrl} component={Account} />
-    <Route path={paths.accountConfirmUrl} component={AccountConfirm} />
-    <Route path={paths.orderHistoryUrl} component={Account} />
-    <Route path={paths.addressBookUrl} component={Account} />
-    <Route path={paths.passwordResetUrl} component={PasswordReset} />
-    <Route path={paths.checkoutUrl} component={CheckoutPage} />
-    <Route path={paths.orderFinalizedUrl} component={ThankYouPage} />
-    <Route component={NotFound} />
-  </Switch>
+    <Switch>
+        <Route exact path={publicURI + paths.baseUrl} component={HomePage} />
+        <Route path={publicURI + paths.searchUrl} component={SearchPage} />
+        <Route path={publicURI + paths.categoryUrl} component={CategoryPage} />
+        <Route path={publicURI + paths.collectionUrl} component={CollectionPage} />
+        <Route path={publicURI + paths.productUrl} component={ProductPage} />
+        <Route path={publicURI + paths.cartUrl} component={CartPage} />
+        <Route path={publicURI + paths.checkoutLoginUrl} component={CheckoutLogin} />
+        <Route path={publicURI + paths.pageUrl} component={ArticlePage} />
+        <Route path={publicURI + accountPaths.baseUrl} component={UserAccount} />
+        <Route path={publicURI + accountPaths.userOrderDetailsUrl} component={OrderDetails} />
+        <Route path={publicURI + paths.guestOrderDetailsUrl} component={OrderDetails} />
+        <Route path={publicURI + paths.accountUrl} component={Account} />
+        <Route path={publicURI + paths.accountConfirmUrl} component={AccountConfirm} />
+        <Route path={publicURI + paths.orderHistoryUrl} component={Account} />
+        <Route path={publicURI + paths.addressBookUrl} component={Account} />
+        <Route path={publicURI + paths.passwordResetUrl} component={PasswordReset} />
+        <Route path={publicURI + paths.checkoutUrl} component={CheckoutPage} />
+        <Route path={publicURI + paths.orderFinalizedUrl} component={ThankYouPage} />
+        <Route component={NotFound} />
+    </Switch>
 );
 
 export default Routes;
