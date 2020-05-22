23f300aac21bc44428bbaa579a5fa6027c895227
dkramskoj <kramskoj.d@brights.cloud>
2020-05-22 17:04:09 +0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+importScripts("precache-manifest.9c292d2eeeba647a28f25a66b761a131.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front"
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [new RegExp("/graphql"), new RegExp("/dashboard")]
+  }
+);
+
